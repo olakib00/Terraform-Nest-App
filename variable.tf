e335860a8c85ec08bdc840ef93ce857859f6618f
olakib00 <iciteystore@gmail.com>
2023-09-26 01:36:40 +0100

    first commit

---
+++ variable.tf
@@ -0,0 +1,14 @@
+# environment variables
+variable "region" {
+  description = "region to create resources"
+  type        = string
+}
+variable "project_name" {
+  description = "project name"
+  type        = string
+}
+
+variable "environment" {
+  description = "environment"
+  type        = string
+}
